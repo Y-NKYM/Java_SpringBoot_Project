7e187887a86061c6d0c13ed2655b33acc086b725
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-12-06 17:56:06 +0900

    [Add] Messages for succeess.

--- src/main/java/com/example/demo/constant/MessageConst.java
+++ src/main/java/com/example/demo/constant/MessageConst.java
@@ -4,6 +4,11 @@ package com.example.demo.constant;
  * メッセージID定数クラス
  */
 public class MessageConst {
-	/** ログイン画面：入力内容誤り */
+	/** ログイン画面 */
+	public static final String LOGIN_SUCCEED = "login.succeed";
 	public static final String LOGIN_FAILED = "login.failed";
+	
+	/** 新規登録画面 */
+	public static final String REGISTER_SUCCEED = "register.succeed";
+	public static final String REGISTER_FAILED = "register.failed";
 }
