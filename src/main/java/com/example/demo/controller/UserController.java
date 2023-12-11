1de6c00bcb7d3c6425259f27627d515ac53cef81
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-12-11 15:42:35 +0900

    [Add] Change view due to user's authority.

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.constant.AuthorityKind;
 import com.example.demo.constant.UrlConst;
 
 @Controller
@@ -11,7 +15,17 @@ import com.example.demo.constant.UrlConst;
 public class UserController {
 	
 	@GetMapping()
-	public String view() {
+	public String view(@AuthenticationPrincipal User authUser, Model model) {
+		
+		/**
+		 * 管理者権限を持っているかをboolean型で判断します。
+		 */
+		boolean hasAdminAuth = authUser.getAuthorities().stream()
+				.allMatch(authority -> authority.getAuthority()
+						.equals(AuthorityKind.ADMIN_AUTHORITY.getAuthorityCode()));
+		model.addAttribute("hasAdminAuth", hasAdminAuth);
+		
+		
 		return "/user/mypage";
 	}
 }
