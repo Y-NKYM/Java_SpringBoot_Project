83b13a93f9afcf47f27cc252db502d7015caef0f
Yuki Nakayama <yuki.nkym.me@gmail.com>
2024-01-04 22:39:50 +0900

    [Add] Create an element/Form to get selected todolist id.

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -4,10 +4,23 @@
 
  $(function(){
 	 $('#todolist tbody tr').on('click', function(){
-		 console.log(this);
+		
+	//選択時のCSS表示
 		$('#todolist tbody tr').removeClass('table-chosen');
 		$(this).addClass('table-chosen');
 	
-		$('#updateTodolist').removeAttr('disabled');
+	//編集ボタンのクリックを可能にする
+		$('#editTodolistBtn').removeAttr('disabled');
+		inputSelectedId($(this));
 	 });
  })
+ 
+ function inputSelectedId(row){
+	 row.find('td').each(function(){
+		 var columnId = $(this).attr('id');
+		 if(columnId.startsWith('id')){
+			 $('#selectedTodolistId').val($(this).text());
+			 return false;
+		 }
+	 });
+ }
